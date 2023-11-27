1d0c8fce3b8eeebac50a9827ac7c4d1e9c5e4e6b
Amasías Rodríguez <100006082+Amasias-Rodriguez@users.noreply.github.com>
2023-11-26 22:14:30 -0600

    Update ImageGenerator.jsx

--- src/Components/ImageGenerator/ImageGenerator.jsx
+++ src/Components/ImageGenerator/ImageGenerator.jsx
@@ -7,11 +7,13 @@ const ImageGenerator = () => {
 
     const [image_url, setImage_url] = useState("/");
     let inputRef = useRef(null);
+    const [loading, setLoading] = useState(false);
 
     const imageGenerator = async () => {
         if (inputRef.current.value === "") {
             return 0;
         }
+        setLoading(true);
         const response = await fetch(
             "https://api.openai.com/v1/images/generations",
             {
@@ -32,12 +34,17 @@ const ImageGenerator = () => {
         let data = await response.json();
         let data_array = data.data;
         setImage_url(data_array[0].url);
+        setLoading(false);
     }
     return (
         <div className='ai-image-generator'>
             <div className="header">Ai image <span>generator</span></div>
             <div className="img-loading">
                 <div className="image"><img src={image_url === "/" ? default_image : image_url} alt="" /></div>
+                <div className="loading">
+                    <div className={loading ? "loading-bar-full" : "loading-bar"}></div>
+                    <div className={loading ? "loading-text" : "display-none"}>Loading....</div>
+                </div>
             </div>
             <div className="search-box">
                 <input type="text" ref={inputRef} className='search-input' placeholder='Describe What you want To see' />
